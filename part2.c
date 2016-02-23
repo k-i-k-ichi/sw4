a3103e4d2b7295ba88334675feb3e169bcccf35b
k-i-k-ichi <k-i-k-ichi@users.noreply.github.com>
2016-02-23 15:30:06 +0000

    File input

--- part2.c
+++ part2.c
@@ -4,8 +4,7 @@ int charcount( FILE *const fin )
 {
     int c, count;
     count = 0;
-    for( ;; )
-    {
+    for( ;; ){
         c = fgetc( fin );
         if( c == EOF || c == '\n' )
             break;
@@ -14,12 +13,15 @@ int charcount( FILE *const fin )
 
     return count;
 }
-
 int main(){
     FILE* fp;
     fp = fopen("check.pol");
     for(int main=0; main<20; main++){
         int lineCharCount = charcount(fp);
+        char* buffer = malloc(lineCharCount*sizeof(char));
+        fseek(fp, lineCharCount, SEEK_CUR); // Rewind to beginning of line
+        
+        
             
     } 
 }
