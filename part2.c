ce35db5578a18414929302af6b7b9273d12265c4
k-i-k-ichi <k-i-k-ichi@users.noreply.github.com>
2016-02-24 00:52:28 +0000

    input parse

--- part2.c
+++ part2.c
@@ -1,27 +1,85 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int charcount( FILE *const fin )
-{
-    int c, count;
-    count = 0;
-    for( ;; ){
-        c = fgetc( fin );
-        if( c == EOF || c == '\n' )
-            break;
-        ++count;
+typedef struct vertice{
+    double x;
+    double y;
+}Vertice;
+
+
+Vertice* parse(char* buffer){
+    int memberCount=0;
+    int i;
+    while(buffer[i]!=NULL){
+        if(buffer[i] == '(') memberCount++;
+        i++;
     }
-
-    return count;
+    Vertice* verticeArray = malloc(memberCount*sizeof(struct vertice));
+    
+    int pointer=1;
+    
+    for(int i=0; i< memberCount;i++){
+        char doubleBuffer[100];
+        int initialPointer = pointer;
+        
+        while(buffer[pointer]!= ','){ // x coordinate
+            doubleBuffer[pointer-initialPointer] = buffer[pointer];
+            pointer++;
+        }
+        pointer+=2; // move past char ','
+        
+        
+        char doubleBuffer2[100];
+        initialPointer = pointer;
+        while(buffer[pointer]!=')'){ // y coordinate
+            doubleBuffer2[pointer-initialPointer] = buffer[pointer];
+            pointer++;
+        }
+        
+        while(buffer[pointer]!='(') pointer++; // seek to next '('
+        
+        pointer++; // skip ( to next number)
+        
+        Vertice* new = malloc(sizeof(struct vertice));
+        new->x = atof(doubleBuffer);
+        new->y = atof(doubleBuffer2);
+        
+        verticeArray[i] = *new;
+    }
+    
+    return verticeArray;
 }
+
+
 int main(){
     FILE* fp;
-    fp = fopen("check.pol");
-    for(int main=0; main<20; main++){
-        int lineCharCount = charcount(fp);
-        char* buffer = malloc(lineCharCount*sizeof(char));
-        fseek(fp, lineCharCount, SEEK_CUR); // Rewind to beginning of line
+    fp = fopen("check.pol", "r");
+    for(int main=0; main<1; main++){
+        char* buffer = malloc(25000*sizeof(char));
+        fgets(buffer, 25000, fp);
         
+        puts(buffer);
+        int i =3;
         
-            
+        char* polygon = malloc(20000*sizeof(char));
+        while(buffer[i] != ';'){
+             polygon[i] = buffer[i];
+             i++;
+        }
+        
+        polygon[i+1]='\0';
+        
+        puts(polygon);
+        
+        i+= 2;
+        char* guard = malloc(8000*sizeof(char));
+        while(buffer[i] != '\n'){
+                guard[i] = buffer[i];
+                i++;
+        }
+        guard[i+1]='\0';
+        free(buffer);
+         
     } 
 }
